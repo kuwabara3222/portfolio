8167b7ad9b6226036863b4a91e0d218fbcc1fedd
DESKTOP-IJ6DPIG\06 <kuwabara3222@gmail.com>
2017-12-11 16:36:14 +0900

    1211

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 $(function () {
-  
-  
-    $('.work-slide').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
+
+
+  $('.work-slide').slick({
+
   });
+
+  $(".menu-trigger").click(function () {
+
+    $(".gnav").stop().slideToggle();
+
+    $(this).toggleClass('active');
+    return false;
+  });
+
+  $navPos = $("#nav").offset().top;
+
+  $(window).scroll(function () {
+    if ($navPos <= $(window).scrollTop()) {
+      $("#nav").addClass("sticky");
+    } else {
+      $("#nav").removeClass("sticky");
+    }
+  })
 })
