8f3282bcf21a3db9e816f6421eef229733c0d032
DESKTOP-IJ6DPIG\06 <kuwabara3222@gmail.com>
2017-12-12 17:07:25 +0900

    1212

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 $(function () {
 
-
+  //slick
   $('.work-slide').slick({
+    infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
     responsive: [{
-        breakpoint: 768,
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+          },
+      {
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
         }
           }
+
           ]
 
   });
 
+  //  ヘッダーロゴ
+  $("#header a").click(function () {
+    $this = $(this).attr("href");
+    target = $($this).offset().top;
+    $("html,body").animate({
+      scrollTop: target - 50
+    }, 1000);
+    return false;
+  })
+
+
+  //  メニューボタン
   $(".menu-trigger").click(function () {
 
     $(".gnav").stop().slideToggle();
@@ -23,13 +44,73 @@ $(function () {
     return false;
   });
 
+  //  ナビゲーションロゴ
+  $(".nav-logo.sp").click(function () {
+    $this = $(this).attr("href");
+    target = $($this).offset().top;
+    $("html,body").animate({
+      scrollTop: target - 50
+    }, 1000);
+    return false;
+  });
+
+
+  //  ナビゲーション
+  $(".gnav li a").click(function () {
+    $(".gnav").stop().slideToggle();
+    $(".menu-trigger").toggleClass('active');
+
+    $this = $(this).attr("href");
+    target = $($this).offset().top;
+    $("html,body").animate({
+      scrollTop: target - 50
+    }, 2000);
+    return false;
+  });
+
+  //  ホバー時のナビゲーション
+  $(".nav-about").hover(function () {
+    $(this).html("私について");
+    $(this).css("font-family", "'游明朝 light',sans-serif");
+  }, function () {
+    $(this).html("ABOUT");
+    $(this).css("font-family", "'Montserrat thin',sans-serif");
+  })
+
+  $(".nav-skill").hover(function () {
+    $(this).html("できること");
+    $(this).css("font-family", "'游明朝 light',sans-serif");
+  }, function () {
+    $(this).html("SKILL");
+    $(this).css("font-family", "'Montserrat thin',sans-serif");
+  })
+
+  $(".nav-work").hover(function () {
+    $(this).html("作品");
+    $(this).css("font-family", "'游明朝 light',sans-serif");
+  }, function () {
+    $(this).html("WORK");
+    $(this).css("font-family", "'Montserrat thin',sans-serif");
+  })
+
+  $(".nav-contact").hover(function () {
+    $(this).html("お問い合わせ");
+    $(this).css("font-family", "'游明朝 light',sans-serif");
+  }, function () {
+    $(this).html("CONTACT");
+    $(this).css("font-family", "'Montserrat thin',sans-serif");
+  })
+
+
+
+
   $navPos = $("#nav").offset().top;
 
   $(window).scroll(function () {
-    if ($navPos <= $(window).scrollTop()) {
+    if ($(window).scrollTop() > $navPos) {
       $("#nav").addClass("sticky");
     } else {
       $("#nav").removeClass("sticky");
     }
-  })
+  });
 })
